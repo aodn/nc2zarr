04ef54120e7c7e93209013c7171b795a8c9b0e3b
Viet Nguyen <vh.nguyen@utas.edu.au>
2022-06-21 11:56:33 +1000

    Add python script to transform Argo NetCDF files

---
+++ utilities/update_argo_yml.py
@@ -0,0 +1,30 @@
+# !/usr/bin/python
+import yaml
+import re
+import subprocess
+import fsspec
+import os
+
+config_file = "./configs/argo/argo_1.yml"
+fs = fsspec.filesystem('s3', anon=True)
+all_files = fs.glob(f's3://imos-data-pixeldrill/viet-test/argo/converted/*.nc')
+os.environ["AWS_PROFILE"] = "nonproduction-admin"
+
+print('*********************************************\n')
+
+for file in all_files:
+    filename = re.search('[\w-]+?(?=\.)', file)[0]
+
+    with open(config_file) as istream:
+        ymldoc = yaml.safe_load(istream)
+        print('Processing file: ', filename)
+        ymldoc['input']['paths'] = re.sub('[\w-]+?(?=\.)', filename, ymldoc['input']['paths'])
+        # regex that replacing last folder from path
+        ymldoc['output']['path'] = re.sub('([^\/]+)(?=.$)', filename, ymldoc['output']['path'])
+
+    with open(config_file, "w") as ostream:
+        yaml.dump(ymldoc, ostream, default_flow_style=False, sort_keys=False)
+
+    subprocess.run(["nc2zarr", "-c", "./configs/argo/argo_1.yml"])
+
+print('\n*********************************************')
